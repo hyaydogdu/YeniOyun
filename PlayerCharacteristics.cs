fa9599784b21042cb7a73eb6c87cdcd25ccc9fc2
hyaydogdu <139559787+hyaydogdu@users.noreply.github.com>
2023-07-14 15:50:15 +0300

    Add files via upload

---
+++ PlayerCharacteristics.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacteristics : MonoBehaviour
+{
+    [SerializeField] private float fallMultiplayer;
+    [SerializeField] private float DragInAir;
+
+    private void FixedUpdate()
+    {
+        updatePhysics(P_Controller._rb, P_Controller._isGrounded);
+    }
+
+    private void updatePhysics(Rigidbody2D rb, bool IsGrounded)
+    {
+        if (rb.velocity.y < 0 && PlayerData._states == PlayerData.states.normal)
+        {
+            rb.gravityScale = fallMultiplayer;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+
+        if (!IsGrounded)
+        {
+            rb.drag = DragInAir;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+}
